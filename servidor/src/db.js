77737a8216167475a79053a6728dba1561cd2ea3
Sebastian George <yoansebastiangeorgemazo@gmail.com>
2024-11-22 11:50:22 -0500

    Add Change

--- servidor/src/db.js
+++ servidor/src/db.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-  try {
-    await mongoose.connect("mongodb+srv://root:1234@projectsiahco.kknzszr.mongodb.net/Auth",{
-      useNewUrlParser: true,
-      useUnifiedTopology:true
-    })
-
-    console.log("DB conectado")
-    
-  } catch (error) {
-    console.log(error)
-    process.exit(1)
-  }
+export const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1/mernlogin");
+        console.log(">> DB Connect")
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+connectDB();
